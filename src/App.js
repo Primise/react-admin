a9ea2c8aa217e4946ef9b934b6a93310b6b23b28
primise <15988402219@163.com>
2019-11-24 23:37:47 +0800

    创建store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import Login from "./components/Login/Login"
-import Home from './components/Home/Home'
+import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom'
+import Login from "./views/Login/Login"
+import Home from './views/Home/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Route  exact path="/home" component={Home}/>
-          <Route  exact path="/login" component={Login}/>
-        </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/"  render={()=><Redirect to="/home/" push />} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ConfigProvider } from 'antd'
+import  "antd/dist/antd.css"
+import zh_CN from "antd/es/locale/zh_CN"
+import {Provider} from 'react-redux'
+import store from './store'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+const AppRoot = (
+  <ConfigProvider locale={zh_CN}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ConfigProvider>
+)
+ReactDOM.render(AppRoot, document.getElementById('root'));
 serviceWorker.unregister();
