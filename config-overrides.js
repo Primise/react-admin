8f6116012b91bbb17ce4b26e29e3c922559d2966
zhangzz <15988402219@163.com>
2019-11-26 09:28:26 +0800

    提交

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const {override,fixBabelImports,addWebpackAlias}  = require('customize-cra');
 const path = require('path')
 
@@ -11,18 +11,5 @@ module.exports = override(
   addWebpackAlias({
     ['@']:path.reslove(__dirname,'src')
   })
-=======
-const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
-const path = require('path')
 
-module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: 'css'
-    }),
-    addWebpackAlias({
-        ['@']: path.resolve(__dirname, 'src')
-    })
->>>>>>> 30eeee5ab3256865b3a2bcc3244945e05026d700
 )
--- src/components/500.js
+++ src/components/500.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
-import img from '../assets/img/500.png'
-=======
 import img from '@/assets/images/500.png'
->>>>>>> 30eeee5ab3256865b3a2bcc3244945e05026d700
 
 const ViewFive = () => (
     <div style={{ textAlign: 'center', marginTop: '5rem' }}>
